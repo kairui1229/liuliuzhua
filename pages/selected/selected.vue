91e18b2cfcce1b5a3c1c1f4bbbe17d1a52b79041
ouykr <kairui1229@qq.com>
2025-09-02 12:20:54 +0800

    精选页面顶部搜索静态结构

--- pages/selected/selected.vue
+++ pages/selected/selected.vue
@@ -1,12 +1,35 @@
 <template>
-	<view>
-		
+	<view class="container">
+		<view class="header">
+			<view class="nav-content">
+				<uni-search-bar
+				 radius="100" 
+				 placeholder="猫粮狗粮、宠物零食" 
+				 clearButton="none" 
+				 cancelButton="none"
+				 style="width: 100%"
+				>
+				</uni-search-bar>
+			</view>
+		</view>
 	</view>
 </template>
 
 <script setup>
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.container{
+	background-color: #f7f7f7;
+	.header{
+		background-color: $uni-color-primary;
+		width: 100%;
+		.nav-content{
+			height: 88rpx;
+			display: flex;
+			align-items: center;
+			padding: 20rpx 50rpx;
+		}
+	}
+}
 </style>
--- unpackage/dist/dev/mp-weixin/pages/selected/selected.js
+++ unpackage/dist/dev/mp-weixin/pages/selected/selected.js
@@ -1,11 +1,25 @@
 "use strict";
 const common_vendor = require("../../common/vendor.js");
 const _sfc_main = {};
+if (!Array) {
+  const _easycom_uni_search_bar2 = common_vendor.resolveComponent("uni-search-bar");
+  _easycom_uni_search_bar2();
+}
+const _easycom_uni_search_bar = () => "../../uni_modules/uni-search-bar/components/uni-search-bar/uni-search-bar.js";
+if (!Math) {
+  _easycom_uni_search_bar();
+}
 function _sfc_render(_ctx, _cache) {
   return {
-    a: common_vendor.gei(_ctx, "")
+    a: common_vendor.p({
+      radius: "100",
+      placeholder: "猫粮狗粮、宠物零食",
+      clearButton: "none",
+      cancelButton: "none"
+    }),
+    b: common_vendor.gei(_ctx, "")
   };
 }
-const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render]]);
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__scopeId", "data-v-6737212f"]]);
 wx.createPage(MiniProgramPage);
 //# sourceMappingURL=../../../.sourcemap/mp-weixin/pages/selected/selected.js.map
