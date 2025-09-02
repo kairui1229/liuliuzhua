3a40c5503939d5f908ba3aab32c53a4bcd678781
ouykr <kairui1229@qq.com>
2025-09-02 16:56:52 +0800

    登录页面基本结构开发

--- unpackage/dist/dev/mp-weixin/pages/login/login.js
+++ unpackage/dist/dev/mp-weixin/pages/login/login.js
@@ -1,16 +1,13 @@
 "use strict";
 const common_vendor = require("../../common/vendor.js");
-const _sfc_main = {
-  data() {
-    return {};
-  },
-  methods: {}
-};
-function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+const common_assets = require("../../common/assets.js");
+const _sfc_main = {};
+function _sfc_render(_ctx, _cache) {
   return {
-    a: common_vendor.gei(_ctx, "")
+    a: common_assets._imports_0$1,
+    b: common_vendor.gei(_ctx, "")
   };
 }
-const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render]]);
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__scopeId", "data-v-e4e4508d"]]);
 wx.createPage(MiniProgramPage);
 //# sourceMappingURL=../../../.sourcemap/mp-weixin/pages/login/login.js.map
