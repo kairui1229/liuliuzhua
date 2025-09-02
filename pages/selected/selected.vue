21a8328dc886bf39084c4fdbfd314bc47baa0fb8
ouykr <kairui1229@qq.com>
2025-09-02 12:41:02 +0800

    获取左侧商品菜单

--- pages/selected/selected.vue
+++ pages/selected/selected.vue
@@ -12,10 +12,33 @@
 				</uni-search-bar>
 			</view>
 		</view>
+		<view class="content">
+			<view class="category-section">
+				<!-- 左侧分类 -->
+				<scroll-view class="category-left" scroll-y>
+				  <view v-for="item in categories" :key="item.id" class="category-item">{{item.category_name}}</view>
+				</scroll-view>
+			</view>
+		</view>
 	</view>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import {onLoad} from "@dcloudio/uni-app"
+import {ref,computed} from "vue"
+import {get} from "@/utils/http"
+
+onLoad(() =>{
+	getCategories()
+})
+
+//获取左侧菜单分类
+const categories:any = ref([])
+const getCategories = async () =>{
+	const data:any = await get("/sel/tag")
+	categories.value = data.tag
+	console.log(categories.value,123)
+}
 </script>
 
 <style lang="scss" scoped>
@@ -29,6 +52,26 @@
 			display: flex;
 			align-items: center;
 			padding: 20rpx 50rpx;
+		}
+	}
+	.content{
+		.category-section{
+			background-color: #fff;
+			border-radius: 16rpx;
+			height: calc(100vh - 150rpx);
+			.category-left{
+				width: 180rpx;
+				height: 100%;
+				background-color: #f7f7f7;
+				.category-item{
+					height: 100rpx;
+					line-height: 100rpx;
+					text-align: center;
+					font-size: 28rpx;
+					color: $uni-text-color;
+					border-bottom: 1px solid #eee;
+				}
+			}
 		}
 	}
 }
--- unpackage/dist/dev/mp-weixin/pages/selected/selected.js
+++ unpackage/dist/dev/mp-weixin/pages/selected/selected.js
@@ -1,6 +1,6 @@
 "use strict";
 const common_vendor = require("../../common/vendor.js");
-const _sfc_main = {};
+const utils_http_index = require("../../utils/http/index.js");
 if (!Array) {
   const _easycom_uni_search_bar2 = common_vendor.resolveComponent("uni-search-bar");
   _easycom_uni_search_bar2();
@@ -9,17 +9,37 @@ const _easycom_uni_search_bar = () => "../../uni_modules/uni-search-bar/componen
 if (!Math) {
   _easycom_uni_search_bar();
 }
-function _sfc_render(_ctx, _cache) {
-  return {
-    a: common_vendor.p({
-      radius: "100",
-      placeholder: "猫粮狗粮、宠物零食",
-      clearButton: "none",
-      cancelButton: "none"
-    }),
-    b: common_vendor.gei(_ctx, "")
-  };
-}
-const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__scopeId", "data-v-6737212f"]]);
+const _sfc_main = /* @__PURE__ */ common_vendor.defineComponent({
+  __name: "selected",
+  setup(__props) {
+    common_vendor.onLoad(() => {
+      getCategories();
+    });
+    const categories = common_vendor.ref([]);
+    const getCategories = async () => {
+      const data = await utils_http_index.get("/sel/tag");
+      categories.value = data.tag;
+      common_vendor.index.__f__("log", "at pages/selected/selected.vue:40", categories.value, 123);
+    };
+    return (_ctx, _cache) => {
+      return {
+        a: common_vendor.p({
+          radius: "100",
+          placeholder: "猫粮狗粮、宠物零食",
+          clearButton: "none",
+          cancelButton: "none"
+        }),
+        b: common_vendor.f(categories.value, (item, k0, i0) => {
+          return {
+            a: common_vendor.t(item.category_name),
+            b: item.id
+          };
+        }),
+        c: common_vendor.gei(_ctx, "")
+      };
+    };
+  }
+});
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["__scopeId", "data-v-6737212f"]]);
 wx.createPage(MiniProgramPage);
 //# sourceMappingURL=../../../.sourcemap/mp-weixin/pages/selected/selected.js.map
