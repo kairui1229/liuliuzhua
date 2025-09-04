600b3ab8c02e624f1eaeee88befc76674789add5
ouykr <kairui1229@qq.com>
2025-09-04 19:38:45 +0800

    地址管理页面静态结构

--- packageB/address/address.vue
+++ packageB/address/address.vue
@@ -1,22 +1,252 @@
 <template>
-	<view>
-		dizhi
+	<view class="address-page">
+		<!--地址列表-->
+		<view class="address-list" v-if="addressList.length">
+			<view class="address-item" v-for="item in addressList" :key="item.id" @click="">
+				<view class="address-info">
+					<view class="contact-info">
+						<text class="name">{{item.name}}</text>
+						<text class="phone">{{item.phone}}</text>
+						<text class="default-tag" v-if="item.is_default">默认</text>
+					</view>
+					<view class="address-detail">
+						{{item.province}}{{item.city}}{{item.district}}{{item.detail}}
+					</view>
+				</view>
+				<view class="address-actions">
+					<view class="action-btn" @click.stop="">
+						<up-icon name="edit-pen" size="20" color="#666" ></up-icon>
+					</view>
+					<view class="action-btn" @click.stop="">
+						<up-icon name="trash" size="20" color="#666"></up-icon>
+					</view>
+				</view>
+			</view>
+		</view>
+		<!--空状态-->
+		<view class="empty-state" v-else>
+			<up-icon name="map" size="60" color="#ccc"></up-icon>
+			<text class="empty-text">您还未添加收货地址哦~</text>
+		</view>
+		<view class="add-btn" @click="">
+			<up-icon name="plus" size="20" color="#fff"></up-icon>
+			<text>新增收货地址</text>
+		</view>
+		<!--地址编辑弹窗-->
+		<up-popup :show="true" closeable class="wrap">
+			<view class="address-form">
+				<view class="form-title">新增地址</view>
+				<view class="form-item">
+					<text class="label">收货人</text>
+					<input placeholder="请输入收货人姓名" class="input"/>
+				</view>
+				<view class="form-item">
+					<text class="label">手机号</text>
+					<input  placeholder="请输入手机号" type="number" maxlength="11" class="input"/>
+				</view>
+				<view class="form-item" @click="">
+					<text class="label">所在地区</text>
+					<view class="region-display">
+						<text >请选择所在地区</text>
+						<up-icon name="arrow-right" size="16" color="#999" class="right"></up-icon>
+					</view>
+				</view>
+				<view class="form-item">
+					<text class="label">详细地址</text>
+					<textarea placeholder="请输入详细地址" class="textarea"/>
+				</view>
+				<view class="form-item">
+					<text class="def">设为默认地址</text>
+					<switch :checked="" color="#ffce2c" @change=""></switch>
+				</view>
+				<view class="form-actions">
+					<view class="cancel-btn" @click="">取消</view>
+					<view class="save-btn" @click="">保存</view>
+				</view>
+			</view>
+		</up-popup>
 	</view>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		methods: {
-			
-		}
+<script lang="ts" setup>
+import { ref,reactive } from "vue"
+import {get,post} from "../../utils/http"
+import {onLoad} from "@dcloudio/uni-app"
+
+onLoad((options)=>{
+	getAddressList()
+})
+
+//地址列表
+const addressList=ref([])
+const getAddressList=async()=>{
+	try {
+		const result:any=await get("/cart/address");
+		addressList.value=result
+		console.log("地址列表",addressList.value)
+	} catch (error) {
+		console.log(error)
 	}
+}
+
 </script>
 
-<style>
+<style lang="scss" scoped>
+	.address-page{
+		min-height: 100vh;
+		background-color: #f5f5f5;
+		padding-bottom: 120rpx;
+		.address-list{
+			.address-item{
+				background-color: #fff;
+				margin-bottom: 20rpx;
+				padding: 30rpx;
+				display: flex;
+				align-items: center;
+				.address-info{
+					flex: 1;
+					.contact-info{
+						margin-bottom: 10rpx;
+						.name{
+							font-size: 32rpx;
+							font-weight: bold;
+							color: #333;
+							margin-right: 20rpx;
+						}
+						.phone{
+							font-size: 28rpx;
+							color: #666;
+							margin-right: 20rpx;
+						}
+						.default-tag{
+							background-color: #ffce2c;
+							color: #fff;
+							font-size: 24rpx;
+							padding: 4rpx 12rpx;
+							border-radius: 6rpx;
+						}
+					}
+					.address-detail{
+						font-size: 28rpx;
+						color: #666;
+						line-height: 1.4;
+					}
+				}
+				
+				.address-actions{
+					display: flex;
+					.action-btn{
+						width: 60rpx;
+						height: 60rpx;
+						display: flex;
+						align-items: center;
+						justify-content: center;
+						background-color: #f5f5f5;
+						border-radius: 50%;
+						margin-right: 16rpx;
+					}
+				}
+			}
+		}
+		.empty-state{
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: center;
+			padding: 100rpx 0;
+			.empty-text{
+				margin-top: 20rpx;
+				font-size: 28rpx;
+				color: #999;
+			}
+		}
+		.add-btn{
+			position: fixed;
+			bottom: 0;
+			left: 0;
+			right: 0;
+			height: 120rpx;
+			background-color: #ffce2c;
+			color: #fff;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			font-size: 32rpx;
+			font-weight: bold;
+			text{margin-left: 16rpx;}
+		}
 
+	}
+	.address-form{
+
+		.form-title{
+			font-size: 36rpx;
+			font-weight: bold;
+			color: #333;
+			text-align: center;
+			margin-bottom: 40rpx;
+			margin-top: 20rpx;
+		}
+		.form-item{
+			display: flex;
+			align-items: center;
+			padding: 30rpx 0;
+			border-bottom: 1rpx solid #eee;
+			.label{
+				width: 160rpx;
+				font-size: 28rpx;
+				color: #333;
+				margin-left: 20rpx;
+			}
+			.input{
+				flex: 1;
+				font-size: 28rpx;
+				color: #333;
+			}
+			.def{margin: 0 20rpx;}
+			.textarea{
+				flex: 1;
+				height: 120rpx;
+				font-size: 28rpx;
+				color: #333;
+			}
+			.region-display{
+				flex: 1;
+				display: flex;
+				align-items: center;
+				justify-content: space-between;
+				font-size: 28rpx;
+				color: #333;
+				text{
+					color: #666
+				}
+				.right{
+					margin-right: 20rpx;
+				}
+			}
+		}
+		.form-actions{
+			display: flex;
+			margin-top:40rpx;
+			.cancel-btn,.save-btn{
+				flex: 1;
+				height: 80rpx;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				font-size: 32rpx;
+				font-weight: bold;
+				border-radius: 40rpx;
+				margin-right: 16rpx;
+			}
+			.cancel-btn{
+				background-color: #f5f5f5;
+				color: #666;
+			}
+			.save-btn{
+				background-color: #ffce2c;
+				color: #fff;
+			}
+		}
+	}
 </style>
--- unpackage/dist/dev/mp-weixin/packageB/address/address.js
+++ unpackage/dist/dev/mp-weixin/packageB/address/address.js
@@ -1,16 +1,101 @@
 "use strict";
 const common_vendor = require("../../common/vendor.js");
-const _sfc_main = {
-  data() {
-    return {};
-  },
-  methods: {}
-};
-function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
-  return {
-    a: common_vendor.gei(_ctx, "")
-  };
+const utils_http_index = require("../../utils/http/index.js");
+if (!Array) {
+  const _easycom_u_icon2 = common_vendor.resolveComponent("u-icon");
+  const _easycom_up_popup2 = common_vendor.resolveComponent("up-popup");
+  (_easycom_u_icon2 + _easycom_up_popup2)();
 }
-const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render]]);
+const _easycom_u_icon = () => "../../uni_modules/uview-plus/components/u-icon/u-icon.js";
+const _easycom_up_popup = () => "../../uni_modules/uview-plus/components/u-popup/u-popup.js";
+if (!Math) {
+  (_easycom_u_icon + _easycom_up_popup)();
+}
+const _sfc_main = /* @__PURE__ */ common_vendor.defineComponent({
+  __name: "address",
+  setup(__props) {
+    common_vendor.onLoad((options) => {
+      getAddressList();
+    });
+    const addressList = common_vendor.ref([]);
+    const getAddressList = async () => {
+      try {
+        const result = await utils_http_index.get("/cart/address");
+        addressList.value = result;
+        common_vendor.index.__f__("log", "at packageB/address/address.vue:97", "地址列表", addressList.value);
+      } catch (error) {
+        common_vendor.index.__f__("log", "at packageB/address/address.vue:99", error);
+      }
+    };
+    return (_ctx, _cache) => {
+      return common_vendor.e({
+        a: addressList.value.length
+      }, addressList.value.length ? {
+        b: common_vendor.f(addressList.value, (item, k0, i0) => {
+          return common_vendor.e({
+            a: common_vendor.t(item.name),
+            b: common_vendor.t(item.phone),
+            c: item.is_default
+          }, item.is_default ? {} : {}, {
+            d: common_vendor.t(item.province),
+            e: common_vendor.t(item.city),
+            f: common_vendor.t(item.district),
+            g: common_vendor.t(item.detail),
+            h: "392a643a-0-" + i0,
+            i: common_vendor.o(() => {
+            }, item.id),
+            j: "392a643a-1-" + i0,
+            k: common_vendor.o(() => {
+            }, item.id),
+            l: item.id,
+            m: common_vendor.o(() => {
+            }, item.id)
+          });
+        }),
+        c: common_vendor.p({
+          name: "edit-pen",
+          size: "20",
+          color: "#666"
+        }),
+        d: common_vendor.p({
+          name: "trash",
+          size: "20",
+          color: "#666"
+        })
+      } : {
+        e: common_vendor.p({
+          name: "map",
+          size: "60",
+          color: "#ccc"
+        })
+      }, {
+        f: common_vendor.p({
+          name: "plus",
+          size: "20",
+          color: "#fff"
+        }),
+        g: common_vendor.o(() => {
+        }),
+        h: common_vendor.p({
+          name: "arrow-right",
+          size: "16",
+          color: "#999"
+        }),
+        i: common_vendor.o(() => {
+        }),
+        j: common_vendor.o(() => {
+        }),
+        k: common_vendor.o(() => {
+        }),
+        l: common_vendor.p({
+          show: true,
+          closeable: true
+        }),
+        m: common_vendor.gei(_ctx, "")
+      });
+    };
+  }
+});
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["__scopeId", "data-v-392a643a"]]);
 wx.createPage(MiniProgramPage);
 //# sourceMappingURL=../../../.sourcemap/mp-weixin/packageB/address/address.js.map
